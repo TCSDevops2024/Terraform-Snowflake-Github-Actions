5e7a70d77c47d143dcddc6b198ebba2f43ddf63c
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-25 13:54:29 +0530

    all project codes

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "snowflake_account" {}
+variable "snowflake_username" {}
+variable "snowflake_password" {}
+variable "snowflake_region" {}
+variable "snowflake_role" {}
+variable "snowflake_database" {}
+variable "snowflake_schema" {}
