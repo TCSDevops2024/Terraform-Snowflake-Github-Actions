50d7616053a4c255abf09fa9d5b80bb7ce853e05
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-25 17:09:31 +0530

    small modify code

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "snowflake" {
   account  = var.snowflake_account
-  user = var.snowflake_username
+  user     = var.snowflake_username
   password = var.snowflake_password
   role     = var.snowflake_role
-  
 }
