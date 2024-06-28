8cbfc79ec0d0ab4bfa450cb903e63e63a10072e4
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-28 14:13:14 +0530

    modify the Repo

--- main.tf
+++ main.tf
@@ -6,8 +6,14 @@ terraform {
     }
   }
 }
+provider "snowflake" {
+  account    = "rp94693.ap-south-1.aws" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT env var
+  user       = "Soumyadip" # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+}
 
-
+provider "snowflake" {
+  profile = "SYS_DBA"
+}
 resource "snowflake_database" "POC-DB" {
   name    = "DB_POC_DevOps-2024"
   comment = "Database for Snowflake Terraform demo"
