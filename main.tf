100fac7266f3beffcc4cbf41c2c5dd18e7801fc2
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-28 16:47:54 +0530

    newly added sql query

--- main.tf
+++ main.tf
@@ -22,14 +22,3 @@ resource "snowflake_schema" "schema" {
   data_retention_days = 1
 }
 
-resource "snowflake_dynamic_table" "demo-table" {
-  name     = "METRICS_T4"
-  database = "DB_POC_DevOps-2024"
-  schema   = "SCH-POC-DevOps-2024"
-  target_lag {
-    maximum_duration = "20 minutes"
-  }
-  warehouse = "WH_FOR_ADMINS"
-  query     = "CREATE TABLE METRICS_T4 (FIRST_NAME VARCHAR(100), LAST_NAME VARCHAR(100), CONTACT_NUMBER VARCHAR(50))"
-  comment   = "sample warehouse "
-}
