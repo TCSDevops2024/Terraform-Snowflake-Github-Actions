95ec046cd81a090af2b79d2b1a353dce84ec3249
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-28 10:20:55 +0530

    modify the Repo

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "account" {
+  description = "Snowflake account"
+}
+variable "username" {
+  description = "Snowflake username"
+}
+variable "region" {
+  description = "Snowflake region"
+}
+variable "password" {
+  description = "Snowflake password"
+}
