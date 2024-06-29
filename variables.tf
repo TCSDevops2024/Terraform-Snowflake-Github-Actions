b08baec0527904643f0f045d03a873ddc848a844
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 11:45:02 +0530

    modified main.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "snowflake_account" {
+  description = "Name of the Snowflake account"
+}
+
+variable "snowflake_profile" {
+  description = "Name of Snowflake profile"
+}
+
+variable "snowflake_password" {
+  description = "Name of Snowflake password"
+}
