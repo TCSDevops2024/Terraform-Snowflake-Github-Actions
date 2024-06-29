3cb43c1a6035dfe3028420a2848cce1d73a9d338
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 14:44:52 +0530

    modified main.tf

--- tables-&-column.tf
+++ tables-&-column.tf
@@ -1,37 +1,53 @@
-provider "snowflake" {
-  account  = var.snowflake_account
-  user     = var.snowflake_user
-  password = var.snowflake_password
-}
-
-resource "snowflake_table" "FDB_POC_TL" {
-  name     = "FDB_POC_TL"
-  schema   = "FDB_SCH_Devops_2024"
-  database = "FDB_POC-Devops_2024"
+resource "snowflake_table_column" "FDB_POC_TL_NAME" {
+  table_name    = "FDB_POC_TL"
+  database_name = "FDB_POC-Devops_2024"
+  schema_name   = "FDB_SCH_Devops_2024"
 
   column {
-    name = "NAME"
-    type = "VARCHAR(255)"
-  }
-
-  column {
-    name = "YOM"
-    type = "INT"
-  }
-
-  column {
-    name = "Model"
-    type = "VARCHAR(20)"
-  }
-
-  column {
-    name = "PRICE"
-    type = "DECIMAL(10, 2)"
-  }
-
-#New columns to be created 
-  column {
-    name = "Distance-covers"
-    type = "DECIMAL(10, 2)"
+    name     = "NAME"
+    type     = "VARCHAR(255)"
+    comment  = "Name of the item"
+    nullable = true
   }
 }
+
+resource "snowflake_table_column" "FDB_POC_TL_YOM" {
+  table_name    = "FDB_POC_TL"
+  database_name = "FDB_POC-Devops_2024"
+  schema_name   = "FDB_SCH_Devops_2024"
+
+  column {
+    name     = "YOM"
+    type     = "INT"
+    comment  = "Year of manufacture"
+    nullable = true
+  }
+}
+
+resource "snowflake_table_column" "FDB_POC_TL_Model" {
+  table_name    = "FDB_POC_TL"
+  database_name = "FDB_POC-Devops_2024"
+  schema_name   = "FDB_SCH_Devops_2024"
+
+  column {
+    name     = "Model"
+    type     = "VARCHAR(20)"
+    comment  = "Model of the item"
+    nullable = true
+  }
+}
+
+resource "snowflake_table_column" "FDB_POC_TL_PRICE" {
+  table_name    = "FDB_POC_TL"
+  database_name = "FDB_POC-Devops_2024"
+  schema_name   = "FDB_SCH_Devops_2024"
+
+  column {
+    name     = "PRICE"
+    type     = "DECIMAL(10, 2)"
+    comment  = "Price of the item"
+    nullable = true
+  }
+}
+
+
