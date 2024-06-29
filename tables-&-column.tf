57500a88041878c6d86ea2c1197cbb79203d41b2
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 14:55:18 +0530

    modified main.tf

--- tables-&-column.tf
+++ tables-&-column.tf
@@ -1,7 +1,3 @@
-resource "snowflake_table_column" "FDB_POC_TL_New_Column" {
-  table_name    = "FDB_POC_TL"
-  database_name = "FDB_POC-Devops_2024"
-  schema_name   = "FDB_SCH_Devops_2024"
 
   column {
     name     = "Distance-covers"
