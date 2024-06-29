f191f5e7995bdd9fa80eb4229b8817da7f7aac95
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 14:41:04 +0530

    modified main.tf

--- tables-&-column.tf
+++ tables-&-column.tf
@@ -1,8 +1,9 @@
 provider "snowflake" {
   account  = var.snowflake_account
-  user = var.snowflake_user
+  user     = var.snowflake_user
   password = var.snowflake_password
 }
+
 resource "snowflake_table" "FDB_POC_TL" {
   name     = "FDB_POC_TL"
   schema   = "FDB_SCH_Devops_2024"
@@ -22,21 +23,15 @@ resource "snowflake_table" "FDB_POC_TL" {
     name = "Model"
     type = "VARCHAR(20)"
   }
+
   column {
     name = "PRICE"
     type = "DECIMAL(10, 2)"
-    
   }
-  #New Columns to be created 
-  
+
+#New columns to be created 
   column {
     name = "Distance-covers"
     type = "DECIMAL(10, 2)"
-    
   }
-  
 }
-
-
-
-
