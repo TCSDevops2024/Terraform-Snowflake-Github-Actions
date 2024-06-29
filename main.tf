a6035ab94a03054ed07f76a0c330daf42835c29a
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 18:14:19 +0530

    new transactions

--- main.tf
+++ main.tf
@@ -44,4 +44,35 @@ resource "snowflake_table" "FDB_Table" {
     nullable = true
     }
 }
-    
+
+# New schema
+resource "snowflake_schema" "SCH_Devops_Github_actions" {
+  database = snowflake_database.FDB_Devops_2024.name
+  name     = "FDB_SCH_Devops_Github_actions"
+  comment  = "A new schema for 2025."
+  is_transient        = false
+  is_managed          = false
+  data_retention_days = 6
+}
+
+# New table
+resource "snowflake_table" "FDB_New_Table" {
+  database = snowflake_database.FDB_Devops_2024.name
+  schema   = snowflake_schema.FDB_SCH_Devops_Github_actions.name
+  name     = "FDB_Devops_TL"
+  comment  = "A new table in the new schema."
+  data_retention_time_in_days = 6
+  change_tracking             = false
+
+  column {
+    name     = "FIRST_NAME"
+    type     = "VARCHAR(255)"
+    nullable = true
+  }
+
+  column {
+    name     = "LAST_NAME"
+    type     = "VARCHAR(255)"
+    nullable = true
+  }
+}    
