6acabecf6b766811c1f08725890b48e78b33b507
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 18:01:53 +0530

    new transactions

--- main.tf
+++ main.tf
@@ -1,24 +1,30 @@
+provider "snowflake" {
+  account  = var.snowflake_account
+  user     = var.snowflake_user
+  password = var.snowflake_password
+
+}
+
 resource "snowflake_database" "FDB_Devops_2024" {
   name                        = "FDB_Devops_2024"
   comment                     = "New DB creation"
   data_retention_time_in_days = 3
 }
 resource "snowflake_schema" "SCH_Devops_2024" {
-  database = "FDB_Devops_2024"
+  database = snowflake_database.FDB_Devops_2024.name
   name     = "FDB_SCH_Devops_2024"
   comment  = "A schema."
-
   is_transient        = false
   is_managed          = false
   data_retention_days = 1
 }
 
 resource "snowflake_table" "FDB_Table" {
-  database                    = snowflake_schema.schema.database
-  schema                      = snowflake_schema.schema.name
+  database                    = snowflake_database.FDB_Devops_2024.name
+  schema                      = snowflake_schema.SCH_Devops_2024.name
   name                        = "FDB_Table"
   comment                     = "A table."
-  data_retention_time_in_days = snowflake_schema.schema.data_retention_time_in_days
+  data_retention_time_in_days = snowflake_schema.SCH_Devops_2024.data_retention_time_in_days
   change_tracking             = false
 
   column {
