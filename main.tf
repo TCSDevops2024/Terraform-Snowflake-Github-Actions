10bdeabc09a9b894237e3c91d0eb24f7432f79d4
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 20:25:07 +0530

    new transactions

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "snowflake" {
   account  = var.snowflake_account
   user     = var.snowflake_user
   password = var.snowflake_password
-
 }
 
 resource "snowflake_database" "FDB_Devops_2024" {
@@ -10,6 +9,7 @@ resource "snowflake_database" "FDB_Devops_2024" {
   comment                     = "New DB creation"
   data_retention_time_in_days = 3
 }
+
 resource "snowflake_schema" "SCH_Devops_2024" {
   database = snowflake_database.FDB_Devops_2024.name
   name     = "FDB_SCH_Devops_2024"
@@ -32,41 +32,16 @@ resource "snowflake_table" "FDB_Table" {
     type     = "VARCHAR"
     nullable = true
   }
+
   column {
-    name     = "VECHILE IDENTITY"
+    name     = "VECHILE_IDENTITY"
     type     = "NUMBER(38,0)"
     nullable = true
-
   }
+
   column {
     name     = "VECHILE_MODEL"
     type     = "VARCHAR"
-    nullable = true
-    }
-}
-
-# New schema
-resource "snowflake_schema" "SCH_Devops_Github_actions" {
-  database = snowflake_database.FDB_Devops_2024.name
-  name     = "FDB_SCH_Devops_Github_actions"
-  comment  = "A new schema for 2025."
-  is_transient        = false
-  is_managed          = false
-  data_retention_days = 6
-}
-
-# New table
-resource "snowflake_table" "FDB_New_Table" {
-  database = snowflake_database.FDB_Devops_2024.name
-  schema   = snowflake_schema.SCH_Devops_Github_actions.name
-  name     = "FDB_Devops_TL"
-  comment  = "A new table in the new schema."
-  data_retention_time_in_days = 6
-  change_tracking             = false
-
-  column {
-    name     = "FIRST_NAME"
-    type     = "VARCHAR(255)"
     nullable = true
   }
 
@@ -75,4 +50,17 @@ resource "snowflake_table" "FDB_New_Table" {
     type     = "VARCHAR(255)"
     nullable = true
   }
-}    
+
+  # New Columns
+  column {
+    name     = "OWNER_NAME"
+    type     = "VARCHAR(255)"
+    nullable = true
+  }
+
+  column {
+    name     = "OWNER_PINCODE"
+    type     = "NUMBER(38,0)"
+    nullable = true
+  }
+}
