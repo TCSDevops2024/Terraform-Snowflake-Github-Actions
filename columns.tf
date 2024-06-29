881ffba207562d43b212ddb896dfe0c2150d252c
Soumya14041987 <soumya14041987@gmail.com>
2024-06-29 19:54:48 +0530

    new transactions

--- columns.tf
+++ columns.tf
@@ -1,9 +1,3 @@
-provider "snowflake" {
-  account   = var.snowflake_account
-  user      = var.snowflake_user
-  password  = var.snowflake_password
-}
-
 resource "snowflake_table_column" "FBD-column" {
   table    = "FDB_Table"
   schema   = "FDB_SCH_Devops_2024"
