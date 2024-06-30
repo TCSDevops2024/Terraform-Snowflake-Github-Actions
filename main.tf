c0c3225bfb91aff2a8e52cb3aa89799e3f72d5f4
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-06-30 18:07:15 +0530

    New columns create

--- main.tf
+++ main.tf
@@ -19,31 +19,3 @@ resource "snowflake_schema" "SCH_Devops_2024" {
   data_retention_days = 3
 }
 
-resource "snowflake_table" "FDB_Table" {
-  database                    = snowflake_database.FDB_Devops_2024.name
-  schema                      = snowflake_schema.SCH_Devops_2024.name
-  name                        = "FDB_Table"
-  comment                     = "A table."
-  data_retention_time_in_days = 3
-  change_tracking             = false
-
-  column {
-    name     = "VECHILE_NAME"
-    type     = "VARCHAR"
-    nullable = true
-  }
-
-  column {
-    name     = "VECHILE_IDENTITY"
-    type     = "NUMBER(38,0)"
-    nullable = true
-  }
-
-  column {
-    name     = "VECHILE_MODEL"
-    type     = "VARCHAR"
-    nullable = true
-  }
-}  
-
-  
