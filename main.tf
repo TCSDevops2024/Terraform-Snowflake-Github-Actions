85876d44509501e858acb7b2db05b47208737186
TCSDevops2024 <chatterjee.soumyadip@tcs.com>
2024-07-01 16:09:20 +0530

    new column

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-provider "snowflake" {
-       account  = var.snowflake_account
-       user     = var.snowflake_user
-       password = var.snowflake_password
-     }
-
-     resource "snowflake_database" "FDB_Devops_2024" {
+resource "snowflake_database" "FDB_Devops_2024" {
        name                        = "FDB_Devops_2024"
        comment                     = "New DB creation"
        data_retention_time_in_days = 4
      }
+
